872ae4e4e5555624647ced79f3dfdd724680b22e
Przemek <ikaria@poczta.onet.pl>
2024-10-18 12:17:22 +0200

    Project setup

---
+++ src/main/java/com/example/lifestyle_data_app/LifestyleDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifestyle_data_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestyleDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestyleDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lifestyle_data_app/LifestyleDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifestyle_data_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifestyleDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
